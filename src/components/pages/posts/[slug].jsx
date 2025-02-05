eaf052165b20c1a59ef7e4e68ee7008ab46e5b2e
ntsakosurprise <mashelesepru@gmail.com>
2025-02-05 15:37:09 +0200

    refactor: remove kotii boiler plate components

--- src/components/pages/posts/[slug].jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AnythingSlug = () => {
-  return <p>Im the AnythingSlug page</p>;
-};
-
-export default AnythingSlug;
--- src/components/pages/test.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Test = () => {
-  return <p>TEST Page</p>;
-};
-
-export default Test;
