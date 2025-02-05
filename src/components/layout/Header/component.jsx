3c0b1f7b7fa03eb3520fdeca82aeb20755e0de5c
ntsakosurprise <mashelesepru@gmail.com>
2025-02-05 16:36:13 +0200

    feat: add link component with ellipse

--- src/components/layout/Header/component.jsx
+++ src/components/layout/Header/component.jsx
@@ -45,7 +45,28 @@ const HeaderShop = styled("div")(() => {
 const StyledLink = styled(Link)({
   display: "flex",
   flexDirection: "row",
+  color: "green"
 });
+const LinkEllipseTextContainer = styled(Link)({
+  display: "flex",
+  flexDirection: "column",
+  gap: 2,
+  alignItems: "center",
+  cursor: "pointer"
+})
+const LinkEllipse = styled("small")({
+  display:"inline-block",
+  width: "15px",
+  height: "15px",
+  backgroundColor: "red",
+  borderRadius: "50%"
+})
+
+const LinkText = styled("small")({
+  display: "inline-block",
+  color:"black",
+  fontWeight: "bold"
+})
 
 const AppHeader = () => {
   return (
@@ -55,7 +76,13 @@ const AppHeader = () => {
         {/* <Link href="https://github.com/ntsakosurprise/kotii" target={"_blank"}>
           <TfiGithub style={{ color: "#00BFA5", fontSize: "25px" }} />
         </Link> */}
-        <StyledLink href="/">Hoe</StyledLink>
+      
+          <LinkEllipseTextContainer href="/">
+            <LinkText>Home</LinkText>
+            <LinkEllipse />
+          </LinkEllipseTextContainer>
+        
+        
         <StyledLink href="/shop">Shop</StyledLink>
         <StyledLink href="/about">About</StyledLink>
         <StyledLink href="/contact">Contact</StyledLink>
@@ -65,6 +92,7 @@ const AppHeader = () => {
       <HeaderCart>
         <StyledLink href="/account">
           <BiUserPlus style={{ color: "#00BFA5", fontSize: "25px" }} />
+         
         </StyledLink>
         <StyledLink href="/search">
           <BiSearch style={{ color: "#00BFA5", fontSize: "25px" }} />
