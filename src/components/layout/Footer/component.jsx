4502fc6b1c9ffc881d3f5ef71a8100a58a970c81
ntsakosurprise <mashelesepru@gmail.com>
2025-02-06 10:16:08 +0200

    feat: attach svg to the footer

--- src/components/layout/Footer/component.jsx
+++ src/components/layout/Footer/component.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import styled from "kotii-styled";
 import FooterActions from "./footeractions.jsx";
 import FooterBrand from "./footerbrand.jsx";
-
+import SvgComponent from "./svgcomp.svg"
 const StyledFooter = styled("footer")({
   marginTop: "auto",
 
   display: "flex",
   flexDirection: "column",
+  position:"relative",
+  paddingBottom: 0,
+  marginBottom: 0
   // justifyItems: "center",
   // alignItems: "center",
 });
@@ -16,8 +19,13 @@ const StyledFooter = styled("footer")({
 const Footer = () => {
   return (
     <StyledFooter>
-      <FooterActions />
-      <FooterBrand />
+      
+      {/* <FooterActions />
+      <FooterBrand /> */}
+      <img src={SvgComponent} width="100%" style={{
+        
+        bottom: 0
+      }}/>
     </StyledFooter>
   );
 };
