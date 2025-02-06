20bf5aa2fd2622f8de0b487a83ae4f798da213ae
ntsakosurprise <mashelesepru@gmail.com>
2025-02-06 10:14:35 +0200

    feat: add image assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,19 @@
 // import connections from "./svgs/connections.svg";
 import SearchImage from "./img/docs_search.png";
 import StonesJPG from "./img/stones.jpg";
-
+import HeroImage from "./img/hero_1.jpg";
+import MiniCapImage from "./img/mini_cap.jpg"
+import BackgroundImage from "./img/background.jpg"
+import CapsImage from "./img/caps.jpg"
+import StackedImage from "./img/stacked.jpg"
+import ManStaring from "./img/man_staring.jpg" 
+import CapHero from "./img/cap-hero.jpg"
 const images = {
   SearchImage,
-  StonesJPG,
+  StonesJPG,HeroImage, 
+  MiniCapImage,BackgroundImage, 
+  CapsImage, StackedImage,
+  ManStaring, CapHero
 };
 // const svgs = {connections}
 const fonts = {};
