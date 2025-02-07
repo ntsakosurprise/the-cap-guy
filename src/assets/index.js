b9803e4679062a8fbbd9815ef0679c699a7e313c
ntsakosurprise <mashelesepru@gmail.com>
2025-02-07 15:51:54 +0200

    feat: export product images

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,6 +17,22 @@ import NewsLetterBackground from "./img/news-letter.jpg"
 import ReferImage from "./img/refer.jpg"
 import Product_1  from "./img/products/product_1.png"
 import Product_2  from "./img/products/product_2.png"
+import Product_3  from "./img/products/product_3.png"
+import Product_4  from "./img/products/product_4.png"
+
+import Product_5  from "./img/products/product_5.png"
+import Product_6  from "./img/products/product_6.png"
+import Product_7  from "./img/products/product_7.png"
+import Product_8  from "./img/products/product_8.png"
+
+import Product_9  from "./img/products/product_9.png"
+import Product_10  from "./img/products/product_10.png"
+import Product_11  from "./img/products/product_11.png"
+import Product_12  from "./img/products/product_12.png"
+
+import Product_13  from "./img/products/product_13.png"
+import Product_14  from "./img/products/product_14.png"
+
 
 const images = {
   SearchImage,
@@ -26,7 +42,10 @@ const images = {
   ManStaring, CapHero, HeroSectionImage,
   CollectionOne, CollectionTwo, CollectionThree,
   CollectionFour, NewsLetterBackground,
-  ReferImage, Product_1, Product_2
+  ReferImage, Product_1, Product_2, Product_3, Product_4,
+  Product_5, Product_6, Product_7, Product_8,
+  Product_9, Product_10, Product_11, Product_12,
+  Product_13, Product_14
 };
 // const svgs = {connections}
 const fonts = {};
